157091f3b4304bb8d172b1dce0762fd58fe4bac7
Ananth <ananth.pro@gmail.com>
2017-06-23 12:11:54 -0400

    printing number of threads

--- hhsim.c
+++ hhsim.c
@@ -184,22 +184,23 @@ void run_sim(double *ps_v,double *rk_v,double *bs_v,double *t_cpu,double *fp_in,
       c0 = (double)clock();
 			#pragma omp parallel
 			{
-			neuron_tm  *nstart, *nend;
-			int tid = omp_get_thread_num();
-			int numthreads = omp_get_num_threads();
-			nstart = nrn + (tid * (numNeurons/numthreads));
-			nend = (nrn + ((tid + 1)* (numNeurons/numthreads)));
-      for(t_ms=0,t=0; t_ms<t_end; t_ms++){
-      	ps_v[t_ms] = nrn[0].v;  //change 0 to index of neuron to be saved
-      	for(step=0; step<steps_ps; step++){
-      		t_next = (double)t_ms + (step+1)*dt;/*end of current time step*/
-      		for(nrnp = nstart; nrnp < nend; nrnp++){ /*loop over neurons*/
-            fp[99] = dt_full;
-            flag = tm_ps(yp,co,yold,ynew,nrnp,fp,dt_full,order_lim);
-    		  } /* end loop over neurons*/
-    		  t=t_next;
-      	} /*loop over steps*/
-      }
+				neuron_tm  *nstart, *nend;
+				int tid = omp_get_thread_num();
+				int numthreads = omp_get_num_threads();
+				printf("There are %d threads", numthreads);
+				nstart = nrn + (tid * (numNeurons/numthreads));
+				nend = (nrn + ((tid + 1)* (numNeurons/numthreads)));
+	      for(t_ms=0,t=0; t_ms<t_end; t_ms++){
+	      	ps_v[t_ms] = nrn[0].v;  //change 0 to index of neuron to be saved
+	      	for(step=0; step<steps_ps; step++){
+	      		t_next = (double)t_ms + (step+1)*dt;/*end of current time step*/
+	      		for(nrnp = nstart; nrnp < nend; nrnp++){ /*loop over neurons*/
+	            fp[99] = dt_full;
+	            flag = tm_ps(yp,co,yold,ynew,nrnp,fp,dt_full,order_lim);
+	    		  } /* end loop over neurons*/
+	    		  t=t_next;
+	      	} /*loop over steps*/
+	      }
 			} /*loop over t_ms*/
       c1 = (double)clock();
       t_cpu[0] = (double)(c1 - c0)/(CLOCKS_PER_SEC);
