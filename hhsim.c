63ff78e3131ee067c5cd18ea09706093c28a2199
Ananth <ananth.pro@gmail.com>
2017-06-23 11:06:56 -0400

    ok testing done

--- hhsim.c
+++ hhsim.c
@@ -336,7 +336,6 @@ int main(int argc, char *argv[]) {
       //calloc initializes all values to 0
   	  double* fp = (double*)calloc(100, sizeof(double));
   	  int* ip = (int*)calloc(100, sizeof(int));
-      printf("Please be only in parallel \n");
   	  ip[0] = 1;
   	  ip[2] = simTime;//Time simulation is run
   	  ip[3] = 1;
